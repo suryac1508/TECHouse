939cfebf4eba1b6f6f7da2be0ff9cf285c72f81d
suryac1508 <43086036+suryac1508@users.noreply.github.com>
2019-12-15 10:20:42 +0530

    Add files via upload

---
+++ smarthome/smarthome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smarthome
+//
+//  Created by Surya Chappidi on 14/12/19.
+//  Copyright © 2019 Surya Chappidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
